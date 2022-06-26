1bda649cff10134c8b58439b513441b81ca0619f
Babatunde Koiki <koikibabatunde14@gmail.com>
2022-06-26 15:36:38 +0100

    set `autoSchemaFile` to a gql file

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     GraphQLModule.forRoot({
-      autoSchemaFile: true,
+      autoSchemaFile: 'schema.gql',
       driver: ApolloDriver,
     }),
     UsersModule,
